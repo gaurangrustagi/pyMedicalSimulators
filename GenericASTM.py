7224fd473139b4485adab94dfa20d00c1935be42
Gaurang <gaurang.rustagi@gmail.com>
2024-06-13 20:05:06 +1000

    used ASTM 1381 class in generic ASTM class

--- GenericASTM.py
+++ GenericASTM.py
@@ -3,12 +3,13 @@
 
 """
 import ASTM1394
+import ASTM1381
 import random
 import datetime
 import time
 	
 def GetXResults( Count ):
-	MSG = ASTM1394.Message()
+	MSG = ASTM1381.Message()
 	MSG.AddLine( 'H|\^&|||^^11223344|||||IDMS||P|1|' + ASTM1394.CurrentDate() )
 	for i in range( 0, Count ):
 		MSG.AddLine( 'P|1||555' )
@@ -23,11 +24,11 @@ def GetXResults( Count ):
 	MSG.AddLine( 'L|1|N' )
 	return MSG
 
-Conn = ASTM1394.Device( '127.0.0.1', 4000 )
+Conn = ASTM1381.Device( '127.0.0.1', 8000 )
 Conn.Open()
 
-ASTM1394.Config_LineNumbering = 1 # include line numbers
-ASTM1394.Config_CheckSum_IncludeSTX = 0 # don't include the <2> in the checksum calculation
+ASTM1381.Config_LineNumbering = 1 # include line numbers
+ASTM1381.Config_CheckSum_IncludeSTX = 0 # don't include the <2> in the checksum calculation
 
 Conn.Send(GetXResults(1))
 
